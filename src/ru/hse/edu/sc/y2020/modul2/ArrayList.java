1f2e1be672874d0962757ef82acca94316f2dbbb
stop-upiter <56906962+stop-upiter@users.noreply.github.com>
2020-11-18 19:27:06 +0300

    от Кости Борисова комментарии

--- src/ru/hse/edu/sc/y2020/modul2/ArrayList.java
+++ src/ru/hse/edu/sc/y2020/modul2/ArrayList.java
@@ -1,12 +1,18 @@
 package ru.hse.edu.sc.y2020.modul2;
 
-public class ArrayList<T>  implements MyList<T> {
+public class ArrayList<T> implements MyList<T> {
+    // Костя: почему DEFAULT_CAPACITY это 0b1010, когда у нас каждый раз она увеличивается в двое (маленькая придирка)
+    // Костя: но то что она сделана константой это хорошо
     private static final int DEFAULT_CAPACITY = 10;
     private T[] array = createArr(DEFAULT_CAPACITY);
     private int size;
 
-    private T[] createArr(int capacity){
-        return (T[])(new Object[capacity]);
+    // Костя: ещё тут идея ругается на то Unchecked cast
+    // Костя: чтобы это исправить надо написать @SuppressWarnings("unchecked")
+    private T[] createArr(int capacity) {
+        // Костя: иногда createArr вызывается с темже capacity, который у нас уже есть
+        // Костя: тут похорошему надо наверно возвращять старый масив
+        return (T[]) (new Object[capacity]);
     }
 
     /**
@@ -16,13 +22,12 @@ public class ArrayList<T>  implements MyList<T> {
      */
     @Override
     public void add(T element) {
-        if (size == array.length){
-            T[] tmp = createArr(size*2);
+        if (size == array.length) {
+            T[] tmp = createArr(size * 2);
             System.arraycopy(array, 0, tmp, 0, size);
             tmp[size] = element;
             array = tmp;
-        }
-        else {
+        } else {
             array[size] = element;
         }
         size++;
@@ -35,34 +40,40 @@ public class ArrayList<T>  implements MyList<T> {
      */
     @Override
     public void removeAt(int index) {
-        if(index<0 || index >=size){
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
 
         int capacity = array.length;
-        if (capacity/2 > size - 1){
-            capacity/=2;
+        if (capacity / 2 > size - 1) {
+            capacity /= 2;
         }
+        // Костя: если этот if несрабатывает, то мы зачамто пересоздаём массив такойже длинны
         T[] tmp = createArr(capacity);
 
         boolean after = false;
 
-        for (int i = 0; i<size; i++){
-            if (i == index){
+        // Костя: тут i надо сравнивать не с size а с size-1 (...)
+        // Костя: ой я незаметил на 61ой строке size--
+        for (int i = 0; i < size; i++) {
+            if (i == index) {
                 after = true;
                 size--;
                 continue;
             }
 
-            if (after){
-                tmp[i] = array[i+1];
-            }
-            else{
+            if (after) {
+                // Костя: (...) потомучто тут тогда будет IndexOutOfRange
+                tmp[i] = array[i + 1];
+            } else {
                 tmp[i] = array[i];
             }
 
         }
         array = tmp;
+
+        // Костя: тут нужен size--
+        // Костя: ой я незаметил на 61ой строке size--
     }
 
     /**
@@ -73,25 +84,27 @@ public class ArrayList<T>  implements MyList<T> {
      */
     @Override
     public boolean remove(T element) {
+        // Костя: почему механизм уменьшение капасити есть только в одном из двух ремувов
         T[] tmp = createArr(array.length);
 
         boolean after = false;
 
-        for (int i = 0; i<size; i++){
-            if (!after && array[i].equals(element)){
+        for (int i = 0; i < size; i++) {
+            if (!after && array[i].equals(element)) {
                 after = true;
                 size--;
                 continue;
             }
 
-            if (after){
-                tmp[i] = array[i+1];
-            }
-            else{
+            if (after) {
+                tmp[i] = array[i + 1];
+            } else {
                 tmp[i] = array[i];
             }
         }
         array = tmp;
+
+        // Костя: почему тут когда нету елемента просто возвращяется false, а не бросается эксепшен
         return after;
     }
 
@@ -103,7 +116,7 @@ public class ArrayList<T>  implements MyList<T> {
      */
     @Override
     public T get(int index) {
-        if(index<0 || index >=size){
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
         return array[index];
@@ -127,8 +140,9 @@ public class ArrayList<T>  implements MyList<T> {
      */
     @Override
     public boolean contains(T element) {
-        for (int i = 0; i<size; i++){
-            if (array[i].equals(element)){
+        // Костя: кажется тут можно было быстрее, если использовать нативный contains у массива
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(element)) {
                 return true;
             }
         }
